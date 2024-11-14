3c758c57d2ff706061bc00c406e3fcd21cc509c2
Sergio-Neto <sergioneto261001@gmail.com>
2024-11-13 21:17:58 -0300

    feat: add person screen and meu sidebar

--- routes/app.routes.tsx
+++ routes/app.routes.tsx
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import PostsList from '@/app/screens/PostsList'
 import PostDetail from '@/app/screens/postDetail'
 import Login from '@/app/screens/login'
+import PersonList from '@/app/screens/personList'
+import PersonForm from '@/app/screens/personForm'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
@@ -11,6 +13,8 @@ export function AppRoutes () {
       <Screen name="Login" component={Login} />
       <Screen name="PostsList" component={PostsList} />
       <Screen name="PostDetail" component={PostDetail} />
+      <Screen name="PersonList" component={PersonList} />
+      <Screen name="PersonForm" component={PersonForm} />
     </Navigator>
   )
 }
